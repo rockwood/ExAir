0c402b2d9dd639be27f70d66cfcf44aa4a6d7635
Kevin Rockwood <kevin@rockwood.me>
2016-09-18 12:20:20 +0900

    Render sensor value on ui index route

--- apps/ui/mix.exs
+++ apps/ui/mix.exs
@@ -20,8 +20,22 @@ defmodule Ui.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Ui, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+    [
+      mod: {Ui, []},
+      applications: applications(Mix.env)
+    ]
+  end
+
+  def applications(_) do
+    [
+      :device,
+      :phoenix,
+      :phoenix_pubsub,
+      :phoenix_html,
+      :cowboy,
+      :logger,
+      :gettext
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -32,11 +46,15 @@ defmodule Ui.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:device, in_umbrella: true, only: :prod},
+      {:device_fake, in_umbrella: true, only: [:dev, :test]},
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 end
--- apps/ui/web/controllers/page_controller.ex
+++ apps/ui/web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Ui.PageController do
   use Ui.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", sensor_value: Device.Sensor.read
   end
 end
